670388f8eb8f5748e18637f9c7399bd2469cba8f
Ryan Miller <rmille70@uwyo.edu>
2024-03-25 17:35:13 -0600

    Fixed linear search issue

--- code.js
+++ code.js
@@ -1,22 +1,29 @@
 // References:
 // https://www.geeksforgeeks.org/binary-search-in-javascript/ for help
-// https://github.com/COSC3020/binary-search-j-halladay5/blob/main/code.js
+// https://github.com/COSC3020/binary-search-ZachRenz/blob/main/code.js
 
 function binarySearch(list, element) {
     var start = 0;
     var end = list.length-1;
-    while(start <= end){
-        var mid = Math.floor((start + end)/2);
-        if(list[mid] === element){
-            while(list[start] != element){
-                start++
+    var index = -1;
+
+    for(i = 0; i < Math.floor(list.length / 2) + 2; i++){
+        piv = Math.floor((end+start)/2);
+        if(list[piv] == element){
+            index = piv;
+            checkDuplicate = false;
+            while(checkDuplicate == false){
+                if(list[piv-1] == element){
+                    index = --piv;
+                } else {
+                    checkDuplicate = true;
+                }
             }
-            return start;
-        } else if (list[mid] < element){
-            start = mid+1;
-        } else {
-            end = mid-1;
+        } else if(list[piv] > element) {
+            end = piv;
+        } else if(list[piv] < element) {
+            start = piv;
         }
     }
-    return -1;
+    return index;
 }
