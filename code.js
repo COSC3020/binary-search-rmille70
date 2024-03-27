d75d3cc539dd005d42a2cffa2eb1048cbba0eca9
Ryan Miller <rmille70@uwyo.edu>
2024-03-27 15:35:10 -0600

    Refined Code + Fixed Recursive Upper Index Error

--- code.js
+++ code.js
@@ -1,40 +1,42 @@
+ 
 // References:
 // https://www.geeksforgeeks.org/binary-search-in-javascript/ for help
 // https://github.com/COSC3020/binary-search-ZachRenz/blob/main/code.js
 // https://www.youtube.com/watch?v=P3YID7liBug (helped for visualization of algorithm)
-
+ 
 function binarySearch(list, element) {
-    let mid;
     if(list.length == 1){
         if(list[0] == element){
             return 0;
-        }
-    }
-    if(list.length % 2 == 0){
-        mid = Math.floor((list.length-1)/2);
-    } else {
-        mid = Math.floor((list.length)/2);
-    }
-
-    
-    if(list[mid] == element){
-        let lower = binarySearch(list.slice(0, mid), element);
-        if(lower == -1){
-            return mid;
         } else {
-            return lower;
+            return -1;
         }
-    } else if(element < list[mid]) {  
-        if(mid == 1){
-            if(list[0] == element){
-                return 0;
+    } else {
+        let mid = Math.floor((list.length-1)/2);
+        if(list[mid] == element){
+            if(binarySearch(list.slice(0, mid), element) == -1){
+                return mid;
+            } else {
+                return binarySearch(list.slice(0, mid), element);
+            }
+        } else if(element < list[mid]) { 
+            return binarySearch(list.slice(0, mid), element);
+        } else if(element > list[mid]){
+            if(binarySearch(list.slice(mid+1, list.length), element) != -1){
+                return ++mid + binarySearch(list.slice(mid, list.length), element);
             } else {
                 return -1;
             }
-        }      
-        return binarySearch(list.slice(0, mid-1), element);
-    } else if(element > list[mid]){
-        return mid+1 + binarySearch(list.slice(mid+1, list.length), element);
+        }
+        return -1;
     }
-    return -1;
 }
+
+/*Testing: 
+console.log(binarySearch([0,1,2,3,4,5,6,7,8,9], -1)) //Should be -1
+console.log(binarySearch([0,1,2,3,4,5,6,7,8,9], 10)) //Should be -1
+console.log(binarySearch([0,1,2,3,4,5,6,7,8,9], 9))  //Should be 9
+console.log(binarySearch([0,1,2,3,4,6,6,7,8,9], 6))  //Should be 5
+console.log(binarySearch([-1,0], 0))                 //Should be 1
+console.log(binarySearch([], 0))                     //Should be -1
+*/
