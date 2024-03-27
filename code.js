9212412d5e368d9830d703ee8946871c45febe41
Ryan Miller <rmille70@uwyo.edu>
2024-03-26 22:32:23 -0600

    Made Recursive binarySearch instead of iterative for uniqueness

--- code.js
+++ code.js
@@ -1,29 +1,41 @@
 // References:
 // https://www.geeksforgeeks.org/binary-search-in-javascript/ for help
 // https://github.com/COSC3020/binary-search-ZachRenz/blob/main/code.js
+// https://www.youtube.com/watch?v=P3YID7liBug (helped for visualization of algorithm)
 
 function binarySearch(list, element) {
-    var start = 0;
-    var end = list.length-1;
-    var index = -1;
-
-    for(i = 0; i < Math.floor(list.length / 2) + 2; i++){
-        piv = Math.floor((end+start)/2);
-        if(list[piv] == element){
-            index = piv;
-            checkDuplicate = false;
-            while(checkDuplicate == false){
-                if(list[piv-1] == element){
-                    index = --piv;
-                } else {
-                    checkDuplicate = true;
-                }
-            }
-        } else if(list[piv] > element) {
-            end = piv;
-        } else if(list[piv] < element) {
-            start = piv;
+    let mid;
+    if(list.length == 1){
+        if(list[0] == element){
+            return 0;
         }
     }
-    return index;
+    if(list.length % 2 == 0){
+        mid = Math.floor((list.length-1)/2);
+    } else {
+        mid = Math.floor((list.length)/2);
+    }
+
+    
+    if(list[mid] == element){
+        console.log("MATCH");
+        let lower = binarySearch(list.slice(0, mid), element);
+        if(lower == -1){
+            return mid;
+        } else {
+            return lower;
+        }
+    } else if(element < list[mid]) {  
+        if(mid == 1){
+            if(list[0] == element){
+                return 0;
+            } else {
+                return -1;
+            }
+        }      
+        return binarySearch(list.slice(0, mid-1), element);
+    } else if(element > list[mid]){
+        return mid+1 + binarySearch(list.slice(mid+1, list.length), element);
+    }
+    return -1;
 }
